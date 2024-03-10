fea3123c24086651280e1dee5e4f8ea43f9ed067
ZarakiLancelot <kenpachi2783@gmail.com>
2024-03-10 01:06:43 -0600

    first commit, splash screen

---
+++ app/src/main/java/com/edesoftech/rememberphant/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.edesoftech.rememberphant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.edesoftech.rememberphant.ui.theme.RememberphantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        Thread.sleep(3000)
+        setContent {
+            RememberphantTheme {
+                // A surface container using the 'background' color from the theme
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RememberphantTheme {
+        Greeting("Android")
+    }
+}
